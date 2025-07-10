352e6c14d38b8c9569e258915b0e838e1b329637
papercrane1001 <papercrane1001@gmail.com>
2025-07-10 10:29:29 -0700

    1.6 wew

---
+++ 1.6/Source/UnAnesthetic/UnAnesthetic/IngestionOutcomeDoer_UnAnesthetic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace UnAnesthetic
+{
+    public class IngestionOutcomeDoer_UnAnesthetic : IngestionOutcomeDoer
+    {
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+            if (pawn == null) { return; }
+            if (pawn.health.hediffSet.HasHediff(HediffDefOf.Anesthetic))
+            {
+                Hediff hb = pawn.health.hediffSet.hediffs.Where(x => x.def == HediffDefOf.Anesthetic).FirstOrDefault();
+                pawn.health.RemoveHediff(hb);
+            }
+        }
+    }
+}
